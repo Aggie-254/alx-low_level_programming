8b6da20e8b3b94266e67ff35022983b180645708
Agnes Mwangangi <agnesmwangangi98@gmail.com>
2023-06-16 15:38:40 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *main - main block
+ *Description: Print alphabet in lowercase
+ *Return:0
+ */
+int main(void)
+{
+	int i;
+	int x;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		x = tolower(i);
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
