a00b69e8db24111938bf733024eba8612aa87162
Agnes Mwangangi <agnesmwangangi98@gmail.com>
2023-06-22 13:42:54 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Agnes Mwangangi
+ * Desc: Header file with prototypes for all functions
+ *       used in the 0x04-more_functions_nested_loops directory.
+ */
+
+int _isupper(int c);
