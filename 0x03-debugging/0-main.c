074edaf8c3f8f41095d3e3849822188dcdaa7960
Agnes Mwangangi <agnesmwangangi98@gmail.com>
2023-06-22 01:50:57 -0700

    change

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,3 +1,5 @@
+
+
 #include "main.h"
 
 /**
@@ -7,10 +9,10 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 98;
-        positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
