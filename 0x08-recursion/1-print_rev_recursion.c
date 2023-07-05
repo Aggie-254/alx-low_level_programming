aa2f4b9918544e32a324e2547f7f9c8764b6f74e
Agnes Mwangangi <agnesmwangangi98@gmail.com>
2023-07-05 13:20:36 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string, followed by a new line
+ * @s: pointer to s.
+ */
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(s[i]);
+}
