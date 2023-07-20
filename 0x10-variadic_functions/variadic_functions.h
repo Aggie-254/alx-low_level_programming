a0f40d4a8e7f747a2739f24994716829b9d23666
Agnes Mwangangi <agnesmwangangi98@gmail.com>
2023-07-20 12:46:18 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,21 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int _putchar(char c)
+int sum_them_all(const unsigned int n, ...);
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
