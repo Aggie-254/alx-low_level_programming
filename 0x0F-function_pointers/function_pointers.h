150197f09734c73115e4f2fd3476d93acb04ce5e
Agnes Mwangangi <agnesmwangangi98@gmail.com>
2023-07-19 22:54:48 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,23 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
